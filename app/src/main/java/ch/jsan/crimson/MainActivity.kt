d02364ecf885080c135fc4e7835651734a8a871f
Jacob Sánchez <jacobszpz@protonmail.com>
2024-06-21 09:31:44 +0100

    Handle media controller connection error and refactor service
    Closes #1
    
    Signed-off-by: Jacob Sánchez <jacobszpz@protonmail.com>

--- app/src/main/java/ch/jsan/crimson/MainActivity.kt
+++ app/src/main/java/ch/jsan/crimson/MainActivity.kt
@@ -1,35 +1,26 @@
 package ch.jsan.crimson
 
-import android.Manifest.permission.BIND_NOTIFICATION_LISTENER_SERVICE
 import android.content.ComponentName
-import android.content.Context
 import android.content.Intent
-import android.media.AudioManager
 import android.os.Build
 import android.os.Bundle
 import android.provider.Settings
-import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.result.ActivityResultLauncher
-import androidx.activity.result.contract.ActivityResultContracts
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults.topAppBarColors
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import ch.jsan.crimson.ui.theme.KingCrimsonTheme
@@ -69,8 +60,6 @@ class MainActivity : ComponentActivity() {
     }
 }
 
-
-
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun MyTopBar() {
@@ -84,7 +73,6 @@ fun MyTopBar() {
         }
     )
 }
-
 
 @Composable
 fun MyScaffold(onClick: () -> Unit) {
@@ -110,7 +98,6 @@ fun MyScaffold(onClick: () -> Unit) {
                 }
             }
         }
-
     }
 }
 
--- app/src/main/java/ch/jsan/crimson/SkipService.kt
+++ app/src/main/java/ch/jsan/crimson/SkipService.kt
@@ -3,84 +3,125 @@ package ch.jsan.crimson
 import android.app.Notification
 import android.os.Build
 import android.os.Bundle
-
-import android.media.session.MediaSession as MediaSession1
-import androidx.media3.session.SessionToken
-import androidx.media3.session.MediaController
-
 import android.service.notification.NotificationListenerService
 import android.service.notification.StatusBarNotification
+import android.util.Log
 import androidx.annotation.OptIn
 import androidx.core.content.ContextCompat
+import androidx.media3.common.MediaMetadata
 import androidx.media3.common.util.UnstableApi
+import androidx.media3.session.MediaController
+import androidx.media3.session.SessionToken
 import com.google.common.util.concurrent.FutureCallback
 import com.google.common.util.concurrent.Futures
-import com.google.common.util.concurrent.ListenableFuture
+import android.media.session.MediaSession as MediaSession1
 
-private const val FORWARD_GAP = 100;
+private const val FORWARD_GAP = 100
+private const val TAG = "SkipService"
 
+/**
+ * Skip YouTube web ads by listening to media notifications
+ *
+ * YouTube ads play out as normal videos, which means they have media controls and
+ * can be fast-forwarded/skipped even manually, this service automates the process
+ *
+ * The ad detection is based on two heuristics:
+ * 1. Video title and channel
+ * 2. Video duration
+ */
 class SkipService : NotificationListenerService() {
-    private val blacklist = listOf<Long>(6041, 14021, 20021, 30061);
+    // Common short ad durations
+    private val blacklist = listOf<Long>(6041, 14021, 20021, 30061)
 
     override fun onListenerConnected() {}
     override fun onListenerDisconnected() {}
     override fun onNotificationRemoved(sbn: StatusBarNotification) {}
 
-    @OptIn(UnstableApi::class) override fun onNotificationPosted(sbn: StatusBarNotification?) {
-        if (sbn == null || !isYouTube(sbn.notification.extras)) return
-        skip(sbn.notification.extras)
-    }
-
-    fun isAdDuration(n: Long) : Boolean {
-        return blacklist.contains(n)
-    }
-
     private fun isYouTube(extras: Bundle) : Boolean {
-        val subText = extras.getString("android.subText")
-        return subText == "m.youtube.com"
+        return extras.getString("android.subText") == "m.youtube.com"
     }
 
-    private fun isAdMetadata(controller: MediaController): Boolean {
-        val text = controller.mediaMetadata.artist.toString().lowercase()
-        val title = controller.mediaMetadata.title.toString().lowercase()
+    /**
+     * Attempt to match the video duration to some common ad durations
+     */
+    private fun isAdDuration(duration: Long) : Boolean {
+        return blacklist.contains(duration)
+    }
+
+    /**
+     * Attempt to match the video title or channel to those used for ads
+     */
+    private fun isAdMetadata(metadata: MediaMetadata): Boolean {
+        val text = metadata.artist.toString().lowercase()
+        val title = metadata.title.toString().lowercase()
 
         return title == "video ad" ||
                 text.startsWith("video ad upload channel for") ||
                 text.startsWith("youtube ads")
     }
 
-    @OptIn(UnstableApi::class) private fun skip(extras: Bundle) {
+    private fun isAd(controller: MediaController): Boolean {
+        return isAdDuration(controller.duration) || isAdMetadata(controller.mediaMetadata)
+    }
+
+    /**
+     * Listen to incoming notifications, obtain their media controller and skip if needed
+     */
+    @OptIn(UnstableApi::class) override fun onNotificationPosted(sbn: StatusBarNotification?) {
+        if (sbn != null && isYouTube(sbn.notification.extras))
+            findController(sbn.notification) { controller ->
+                Log.d(TAG, "Checking video: " + controller.duration)
+
+                if (isAd(controller)) {
+                    // Avoid getting stuck in a loop
+                    if (controller.currentPosition < (controller.duration - FORWARD_GAP)) {
+                        Log.d(TAG, "FAST FORWARDING")
+
+                        // fast-forward
+                        controller.seekTo(controller.duration - FORWARD_GAP)
+                    }
+                }
+            }
+    }
+
+    /**
+     * Extract token from notification and attempt connection to media controller
+     */
+    @OptIn(UnstableApi::class) private fun findController(notification: Notification, onSuccess: (MediaController) -> Unit) {
         val mediaSessionToken = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
-            extras.getParcelable(Notification.EXTRA_MEDIA_SESSION, MediaSession1.Token::class.java)
+            notification.extras.getParcelable(Notification.EXTRA_MEDIA_SESSION, MediaSession1.Token::class.java)
         } else {
             @Suppress("DEPRECATION")
-            extras.getParcelable(Notification.EXTRA_MEDIA_SESSION)
+            notification.extras.getParcelable(Notification.EXTRA_MEDIA_SESSION)
         }
 
         if (mediaSessionToken == null) return
 
-        val tokenFuture: ListenableFuture<SessionToken> = SessionToken.createSessionToken(this, mediaSessionToken)
+        val tokenFuture = SessionToken.createSessionToken(this, mediaSessionToken)
+
         Futures.addCallback(tokenFuture, object : FutureCallback<SessionToken> {
             override fun onSuccess(token: SessionToken) {
                 val controllerFuture = MediaController.Builder(baseContext, token).buildAsync()
-                controllerFuture.addListener({
-                    val controller = controllerFuture.get()
-                    println(controller.duration)
 
-                    if (controller.currentPosition < (controller.duration -  FORWARD_GAP)) {
-                        if (isAdDuration(controller.duration) || isAdMetadata(controller)) {
-                            println("FAST FORWARDING")
-
-                            // fast-forward
-                            controller.seekTo(controller.duration - FORWARD_GAP)
-                        }
+                Futures.addCallback(controllerFuture, object: FutureCallback<MediaController> {
+                    override fun onSuccess(controller: MediaController) {
+                        onSuccess(controller)
                     }
 
-                }, ContextCompat.getMainExecutor(baseContext))
+                    override fun onFailure(t: Throwable) {
+                        Log.w(TAG, "Could not connect to the media3 session")
+                    }
+                },
+                    if (Build.VERSION.SDK_INT > Build.VERSION_CODES.P) {
+                        mainExecutor
+                    } else {
+                        ContextCompat.getMainExecutor(baseContext)
+                    }
+                )
             }
 
             override fun onFailure(t: Throwable) {
-                println("Could not resolve media3 session token")
+                Log.w(TAG, "Could not create media3 session token")
             }
         },
             if (Build.VERSION.SDK_INT > Build.VERSION_CODES.P) {
@@ -91,4 +132,3 @@ class SkipService : NotificationListenerService() {
         )
     }
 }
-
